98cbd95226474e9d4afa1af312758369d9ad8586
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-03-06 20:07:18 +0200

    Udate project structure

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,12 +6,12 @@ import { CoreModule } from 'src/app/core/core.module'
 import { LayoutModule } from 'src/app/shared/modules/layout/layout.module'
 
 import { HomeRoutingModule } from 'src/app/modules/home/home-routing.module'
-import { HomeHeroComponent } from 'src/app/modules/home/pages/home/home-start/home-start.component'
-import { HomeProjectsComponent } from 'src/app/modules/home/pages/home/home-start/home-projects/home-projects.component'
+import { HomeHeroComponent } from 'src/app/modules/home/pages/home/home-hero/home-hero.component'
+import { HomeProjectsComponent } from 'src/app/modules/home/pages/home/home-projects/home-projects.component'
 import { HomeComponent } from 'src/app/modules/home/pages/home/home.component'
-import { NewsComponent } from './pages/home/home-start/home-projects/news/news.component'
-import { PresentationComponent } from './pages/home/home-start/home-projects/presentation/presentation.component'
-import { InfoComponent } from './pages/home/home-start/home-projects/info/info.component'
+import { NewsComponent } from './pages/home/home-projects/news/news.component'
+import { PresentationComponent } from './pages/home/home-projects/presentation/presentation.component'
+import { InfoComponent } from './pages/home/home-projects/info/info.component'
 
 @NgModule({
   declarations: [HomeComponent, HomeHeroComponent, HomeProjectsComponent, NewsComponent, PresentationComponent, InfoComponent],
--- src/app/modules/home/pages/home/home-start/home-start.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-home-start',
-  templateUrl: './home-start.component.html',
-  styleUrls: ['./home-start.component.scss'],
-})
-export class HomeHeroComponent {}
