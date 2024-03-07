8eefdf0788bc720fbce4e3c52bd8660326ca1d91
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-03-07 16:29:33 +0200

    Update style and add anchor link

--- src/app/modules/home/pages/home/home-hero/home-hero.component.ts
+++ src/app/modules/home/pages/home/home-hero/home-hero.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit } from '@angular/core'
-// import * as Aos from 'aos'
 @Component({
   selector: 'app-home-hero',
   templateUrl: './home-hero.component.html',
   styleUrls: ['./home-hero.component.scss'],
 })
-export class HomeHeroComponent implements OnInit {
-  ngOnInit(): void {
-    // Aos.init()
-  }
-}
+export class HomeHeroComponent {}
